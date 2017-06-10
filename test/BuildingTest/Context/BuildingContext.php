cc0d3aa88cf73f767cc7f69b37f5643b5ed973bf
Marco Pivetta <ocramius@gmail.com>
2017-06-10 17:10:05 +0300

    Adding implementation for the first scenario of check-in-check-out

--- test/BuildingTest/Context/BuildingContext.php
+++ test/BuildingTest/Context/BuildingContext.php
@@ -4,16 +4,50 @@ declare(strict_types=1);
 
 namespace BuildingTest\Context;
 
+use Assert\Assertion;
 use Behat\Behat\Context\Context;
+use Building\Domain\Aggregate\Building;
+use Building\Domain\DomainEvent\NewBuildingWasRegistered;
+use Building\Domain\DomainEvent\UserCheckedIn;
+use Prooph\EventSourcing\AggregateChanged;
+use Rhumsaa\Uuid\Uuid;
 
 final class BuildingContext implements Context
 {
+    /**
+     * @var AggregateChanged[]
+     */
+    private $pastEvents     = [];
+
+    /**
+     * @var null|AggregateChanged[]
+     */
+    private $recordedEvents;
+
+    /**
+     * @var Building|null
+     */
+    private $building;
+
+    /**
+     * @var Uuid
+     */
+    private $aggregateId;
+
+    public function __construct()
+    {
+        $this->aggregateId = Uuid::uuid4();
+    }
+
     /**
      * @Given a building was registered
      */
     public function aBuildingWasRegistered() : void
     {
-        throw new \BadMethodCallException('Not implemented');
+        $this->record(NewBuildingWasRegistered::occur(
+            $this->aggregateId->toString(),
+            ['name' => 'foo']
+        ));
     }
 
     /**
@@ -21,14 +55,48 @@ final class BuildingContext implements Context
      */
     public function aUserChecksIntoTheBuilding() : void
     {
-        throw new \BadMethodCallException('Not implemented');
+        $this->building()->checkInUser('Mr. Magoo');
     }
 
     /**
      * @Then the user should have checked into the building
+     *
+     * @throws \Assert\AssertionFailedException
      */
     public function theUserShouldHaveCheckedIntoTheBuilding() : void
     {
-        throw new \BadMethodCallException('Not implemented');
+        /* @var $event UserCheckedIn */
+        $event = $this->getNextRecordedEvent();
+
+        Assertion::isInstanceOf($event, UserCheckedIn::class);
+        Assertion::same($event->username(), 'Mr. Magoo');
+    }
+
+    private function record(AggregateChanged $event) : void
+    {
+        $this->pastEvents[] = $event;
+    }
+
+    private function building() : Building
+    {
+        return $this->building
+            ?? $this->building = \Closure::bind(function (array $events) {
+                return Building::reconstituteFromHistory(new \ArrayIterator($events));
+            }, null, Building::class)
+                ->__invoke($this->pastEvents);
+    }
+
+    private function getNextRecordedEvent() : AggregateChanged
+    {
+        if (null === $this->recordedEvents) {
+            $building = $this->building();
+            $popEvents = new \ReflectionMethod($building, 'popRecordedEvents');
+
+            $popEvents->setAccessible(true);
+
+            $this->recordedEvents = $popEvents->invoke($building);
+        }
+
+        return array_shift($this->recordedEvents);
     }
 }
