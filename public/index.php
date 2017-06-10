95f464d7bd8010bf4620100be0df9e02c58c95ec
Marco Pivetta <ocramius@gmail.com>
2017-06-10 14:02:41 +0300

    Symmetrically to check-in, here's the check-out functionality

--- public/index.php
+++ public/index.php
@@ -76,11 +76,18 @@ call_user_func(function () {
         );
 
         return $response->withAddedHeader('Location', '/building/' . $buildingId->toString());
-
     });
 
     $app->post('/checkout/{buildingId}', function (Request $request, Response $response) use ($sm) : Response {
+        $buildingId = Uuid::fromString($request->getAttribute('buildingId'));
 
+        $commandBus = $sm->get(CommandBus::class);
+        $commandBus->dispatch(Command\CheckOutUser::with(
+            $buildingId,
+            $request->getParsedBody()['username'])
+        );
+
+        return $response->withAddedHeader('Location', '/building/' . $buildingId->toString());
     });
 
     $app->pipeDispatchMiddleware();
